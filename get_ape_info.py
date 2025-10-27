e6e490c01690a7c57f0b84fabe405f27e5de7b5a
Katarina <24740448@qq.com>
2025-10-27 01:07:38 -0700

    Update get_ape_info.py

--- get_ape_info.py
+++ get_ape_info.py
@@ -25,7 +25,7 @@ contract = web3.eth.contract(address=contract_address, abi=abi)
 def _fetch_ipfs_json(uri: str) -> dict:
     if uri.startswith("ipfs://"):
         path = uri[7:]
-        uri = f"green-used-gerbil-851.mypinata.cloud/ipfs/QmXAwcEBr4NgoFSkQLpv39vdAamWLrSiKynRdjmrHUbDSG"
+        uri = f"https://ipfs.io/ipfs/QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/1"
 
     response = requests.get(uri, timeout=20)
     response.raise_for_status()

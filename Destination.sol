c89de9027c5485829c966c3318ccaaf7541cadd9
Katarina <24740448@qq.com>
2025-11-08 23:23:09 -0800

    Update Destination.sol

--- Destination.sol
+++ Destination.sol
@@ -25,41 +25,41 @@ contract Destination is AccessControl {
 	function wrap(address _underlying_token, address _recipient, uint256 _amount ) public onlyRole(WARDEN_ROLE) {
 		//YOUR CODE HERE
 		require(_recipient != address(0), "bad recipient");
-    require(_amount > 0, "zero amount");
-
-    address wrapped = underlying_tokens[_underlying_token];
-    require(wrapped != address(0), "token not registered");
-
-    BridgeToken(wrapped).mint(_recipient, _amount);
-
-    emit Wrap(_underlying_token, wrapped, _recipient, _amount);
+	    require(_amount > 0, "zero amount");
+	
+	    address wrapped = underlying_tokens[_underlying_token];
+	    require(wrapped != address(0), "token not registered");
+	
+	    BridgeToken(wrapped).mint(_recipient, _amount);
+	
+	    emit Wrap(_underlying_token, wrapped, _recipient, _amount);
 	}
 
 	function unwrap(address _wrapped_token, address _recipient, uint256 _amount ) public {
 		//YOUR CODE HERE
-	  require(_recipient != address(0), "bad recipient");
-    require(_amount > 0, "zero amount");
-
-    address underlying = wrapped_tokens[_wrapped_token];
-    require(underlying != address(0), "unknown wrapped token");
-
-    BridgeToken(_wrapped_token).burnFrom(msg.sender, _amount);
-
-    emit Unwrap(underlying, _wrapped_token, msg.sender, _recipient, _amount);
+	    require(_recipient != address(0), "bad recipient");
+	    require(_amount > 0, "zero amount");
+	
+	    address underlying = wrapped_tokens[_wrapped_token];
+	    require(underlying != address(0), "unknown wrapped token");
+	
+	    BridgeToken(_wrapped_token).burnFrom(msg.sender, _amount);
+	
+	    emit Unwrap(underlying, _wrapped_token, msg.sender, _recipient, _amount);
 	}
 
 	function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {
 		//YOUR CODE HERE
-		  require(_underlying_token != address(0), "bad underlying");
-    require(underlying_tokens[_underlying_token] == address(0), "already registered");
-    address wrapped = address(new BridgeToken(_underlying_token, name, symbol, address(this)));
-
-    underlying_tokens[_underlying_token] = wrapped;
-    wrapped_tokens[wrapped] = _underlying_token;
-    tokens.push(wrapped);
-
-    emit Creation(_underlying_token, wrapped);
-    return wrapped;
+		require(_underlying_token != address(0), "bad underlying");
+	    require(underlying_tokens[_underlying_token] == address(0), "already registered");
+	    address wrapped = address(new BridgeToken(_underlying_token, name, symbol, address(this)));
+	
+	    underlying_tokens[_underlying_token] = wrapped;
+	    wrapped_tokens[wrapped] = _underlying_token;
+	    tokens.push(wrapped);
+	
+	    emit Creation(_underlying_token, wrapped);
+	    return wrapped;
 	}
 
 }
